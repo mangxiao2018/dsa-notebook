069f876915435b0ad1aa1c9e5592c4b9fcc761b4
mangxiao <mangxiao2018@126.com>
2021-04-05 11:05:59 +0800

    数据栈-增加peek及计算器所用相关方法

--- 代码/dsasample/src/main/java/com/mangxiao/datastructures/stack/ArrayStack.java
+++ 代码/dsasample/src/main/java/com/mangxiao/datastructures/stack/ArrayStack.java
@@ -15,6 +15,14 @@ public class ArrayStack {
         stack = new int[maxSize];
     }
 
+    /**
+     * 返回当前栈的值
+     * @return
+     */
+    public int peek(){
+        return stack[top];
+    }
+
     public boolean isFull(){
         return top == maxSize - 1;
     }
@@ -49,5 +57,58 @@ public class ArrayStack {
         for (int i=top; i>= 0; i--){
             System.out.printf("stack[%d]=%d\n",i, stack[i]);
         }
+    }
+
+    /**
+     * 返回运算符的优先级，使用数字表示
+     * 数字越大，则优先级越高
+     * @param oper
+     * @return
+     */
+    public int priority(int oper){
+        if (oper == '*' || oper == '/'){
+            return 1;
+        } else if (oper == '+' || oper == '-'){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
+    /**
+     * 判断是不是一个运算符
+     * @param val
+     * @return
+     */
+    public boolean isOper(char val){
+        return val == '+' || val == '-' || val == '*' || val == '/';
+    }
+
+    /**
+     * 计算方法
+     * @param num1
+     * @param num2
+     * @param oper
+     * @return
+     */
+    public int cal(int num1, int num2, int oper){
+        int res = 0;
+        switch (oper){
+            case '+':
+                res = num1 + num2;
+                break;
+            case '-':
+                res = num2 - num1;
+                break;
+            case '*':
+                res = num1 * num2;
+                break;
+            case '/':
+                res = num2 / num1;
+                break;
+            default:
+                break;
+        }
+        return res;
     }
 }
